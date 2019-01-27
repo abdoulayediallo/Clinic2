fada7873e2d14eff9666ba08238762b36d7797ed
Diallo Abdoulaye <d_abdoulaye@outlook.com>
2019-01-27 16:47:18 +0100

    Add project files.

---
+++ Clinic2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clinic2.Startup))]
+namespace Clinic2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
